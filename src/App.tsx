0a2824ea771d729f03ef89741875415eaa30106d
siamso <hello.siamahamed@gmail.com>
2025-04-24 05:14:21 +0600

    project initializing

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+/**
+ * @copyright 2025 siamso
+ * @license Apache-2.0
+ */
+import React from 'react';
+
+const App = () => {
+  return <div>app</div>;
+};
+
+export default App;
---
+++ src/main.tsx
@@ -0,0 +1,14 @@
+/**
+ * @copyright 2025 siamso
+ * @license Apache-2.0
+ */
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.tsx';
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
