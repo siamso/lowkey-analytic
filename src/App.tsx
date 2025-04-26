4574a6455c538a2941eddd7709e57ab7cb75f17c
siamso <hello.siamahamed@gmail.com>
2025-04-27 04:54:22 +0600

    Hero section with motions

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,22 @@
  * @copyright 2025 siamso
  * @license Apache-2.0
  */
+
 import React from 'react';
-import Header from './components/Header';
+import Header from '@/components/Header';
+import Hero from '@/components/Hero';
+import { ReactLenis } from 'lenis/react';
 
 const App = () => {
   return (
-    <>
-      <Header />
-    </>
+    <ReactLenis root>
+      <div className='relative isolate overflow-hidden'>
+        <Header />
+        <main>
+          <Hero />
+        </main>
+      </div>
+    </ReactLenis>
   );
 };
 
